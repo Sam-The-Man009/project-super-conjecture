618446f5c2216a01c6ba2722b194f42cb4e6fd67
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-07 15:00:06 +0200

    lol update

--- flake.nix
+++ flake.nix
@@ -1,46 +1,50 @@
-#{ inputs, outputs, ... }:
 {
+  description = "Dynamic NixOS configuration for heterogeneous systems";
+
   inputs = {
     # Nixpkgs: The Nix Packages collection.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
-      systemNames = ["sys1" "sys2" "sys3" "sys4" "sys5" "sys6" "sys7" "sys8" "sys9" "sys10"
+      systemNames = [
+        "sys1" "sys2" "sys3" "sys4" "sys5" "sys6" "sys7" "sys8" "sys9" "sys10"
         "sys11" "sys12" "sys13" "sys14" "sys15" "sys16" "sys17" "sys18" "sys19" "sys20"
-        "sys21" "sys22" "sys23" "sys24" "sys25"];
+        "sys21" "sys22" "sys23" "sys24" "sys25"
+      ];
 
       types = {
         master = { config, pkgs, ... }: {
-          nixosConfigurations.master = import ./types/master/imports.nix { inherit (config) configuration; };
+          nixosConfigurations.master = import ./types/master/imports.nix { inherit config pkgs; };
           homeManagerConfiguration = import ./types/master/home.nix;
         };
 
         node = { config, pkgs, ... }: {
-          nixosConfigurations.node = import ./types/node/imports.nix { inherit (config) configuration; };
+          nixosConfigurations.node = import ./types/node/imports.nix { inherit config pkgs; };
           homeManagerConfiguration = import ./types/node/home.nix;
         };
 
         user = { config, pkgs, ... }: {
-          nixosConfigurations.user = import ./types/user/imports.nix { inherit (config) configuration; };
+          nixosConfigurations.user = import ./types/user/imports.nix { inherit config pkgs; };
           homeManagerConfiguration = import ./types/user/home.nix;
         };
       };
 
     in
     {
-      description = "Dynamic NixOS configuration distributior for heterogeneous systems, combining role-based defaults with host-specific customizations. For purposes of global domination and computing aggregation.";
+      nixosConfigurations = {
+        # Dynamically create NixOS configurations for each system
+        inherit (types) master node user;
+      };
 
-      imports = [ self.inputs.common ];
-
-      types = types;
-
-      hosts = systemNames.map (systemName: {
-        nixosConfigurations.${systemName} = import ./hosts/${systemName}/unique.nix;
-        #homeManagerConfiguration = import ./hosts/${systemName}/home.nix;
-      });
-
-    }
+      # Define configurations for each system
+      hosts = builtins.listToAttrs (
+        builtins.map (systemName: {
+          name = systemName;
+          value = import ./hosts/${systemName}/unique.nix { inherit systemName nixpkgs home-manager; };
+        }) systemNames
+      );
+    };
 }
