18437e849525680a8f0d3c8425bd015cb3bede15
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-14 15:39:13 +0200

    a

--- flake.nix
+++ flake.nix
@@ -11,7 +11,31 @@
       system = "x86_64-linux";  # Specify your target system architecture
       pkgs = import nixpkgs { system = system; };
 
-      # Define system types with associated module imports
+      # Function to generate a configuration for a given system name and type
+      generateConfig = systemName: systemType: {
+        name = "${systemName}-${systemType}";  # Construct a key like "sys1-user"
+        value = pkgs.lib.nixosSystem {
+          system = system;
+          modules = [
+            (import ./configuration.nix)
+            (import ./types/${systemType}/imports.nix { inherit pkgs; })
+            (import ./types/${systemType}/home.nix)
+          ];
+          configuration = {
+            # Add additional configurations here
+            networking.hostName = "${systemName}-${systemType}";
+            # Any additional configuration settings can be included here
+          };
+        };
+      };
+
+      # Define system names and types
+      systemNames = [
+        "sys1" "sys2" "sys3" "sys4" "sys5" "sys6" "sys7" "sys8" "sys9" "sys10"
+        "sys11" "sys12" "sys13" "sys14" "sys15" "sys16" "sys17" "sys18" "sys19"
+        "sys20" "sys21" "sys22" "sys23" "sys24" "sys25"
+      ];
+
       types = {
         master = { config, pkgs, ... }: {
           imports = import ./types/master/imports.nix { inherit config pkgs; };
@@ -28,39 +52,6 @@
           homeManager = import ./types/user/home.nix;
         };
       };
-
-      # Function to generate NixOS configurations
-      generateConfig = systemName: systemType: {
-        name = "${systemName}-${systemType}";  # Construct a key like "sys1-user"
-        value = pkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-            ({ pkgs, ... }: {
-              imports = [];
-              hostname = "${systemName}-${systemType}";
-              # Other default settings or configurations here
-            })
-            (let
-              hostType = types.${systemType} { config = {}; pkgs = pkgs; };
-            in {
-              imports = [ hostType.imports ];
-              hostname = "${systemName}-${systemType}";
-              modules = [ home-manager.nixosModules.home-manager hostType.homeManager ];
-            })
-          ];
-          configuration = {
-            # Additional configurations here if needed
-            networking.hostName = "${systemName}-${systemType}";
-          };
-        };
-      };
-
-      # Define system names
-      systemNames = [
-        "sys1" "sys2" "sys3" "sys4" "sys5" "sys6" "sys7" "sys8" "sys9" "sys10"
-        "sys11" "sys12" "sys13" "sys14" "sys15" "sys16" "sys17" "sys18" "sys19"
-        "sys20" "sys21" "sys22" "sys23" "sys24" "sys25"
-      ];
 
     in {
       nixosConfigurations = builtins.listToAttrs (
