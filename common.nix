95f72f6bb64191903723888a02bc3fc8b33c0a8c
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-15 09:59:59 +0200

    added filesystem specs

--- common.nix
+++ common.nix
@@ -14,4 +14,8 @@
   i18n.defaultLocale = "en_DK.UTF-8";
   console.keyMap = "dk-latin1";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/XXXX-XXXX";
+    fsType = "ext4";  # Change to your filesystem type
+  };
 }
