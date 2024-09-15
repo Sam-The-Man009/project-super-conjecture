6ce885f89959e09931975a528c7f3575b2189f47
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-15 11:50:27 +0200

    remaking the flakes file into a configuration.nix + added users

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,65 @@
 { config, pkgs, ... }:
-{
-  nixpkgs.config.allowUnfree = true;
+
+let
+  system = builtins.currentSystem;
+
+  # Retrieve the current username from the environment
+  username = builtins.getEnv "USER";  # assumes the environment variable USER is set
+  
+  # List of valid system names
+  systemNames = [
+    "sys1" "sys2" "sys3" "sys4" "sys5" "sys6" "sys7" "sys8" "sys9" "sys10"
+    "sys11" "sys12" "sys13" "sys14" "sys15" "sys16" "sys17" "sys18" "sys19"
+    "sys20" "sys21" "sys22" "sys23" "sys24" "sys25"
+  ];
+
+  # Definition of types
+  types = {
+    master = { config, pkgs, ... }: {
+      imports = import ./types/master/imports.nix { inherit config pkgs; };
+      homeManager = import ./types/master/home.nix;
+    };
+
+    node = { config, pkgs, ... }: {
+      imports = import ./types/node/imports.nix { inherit config pkgs; };
+      homeManager = import ./types/node/home.nix;
+    };
+
+    user = { config, pkgs, ... }: {
+      imports = import ./types/user/imports.nix { inherit config pkgs; };
+      homeManager = import ./types/user/home.nix;
+    };
+  };
+
+  # Split the username into systemName and systemType
+  parts = builtins.split "-" username;
+
+  # Check if the username is correctly split into two parts
+  _ = assert builtins.length parts == 2
+    "Username must be in the format 'systemName-systemType'.";
+
+  systemName = builtins.elemAt parts 0;  # First part is systemName
+  systemType = builtins.elemAt parts 1;  # Second part is systemType
+
+  # Assertions to validate that systemName and systemType are valid
+  _ = assert builtins.elem systemName systemNames;
+    "Invalid systemName: ${systemName}. It must be one of ${systemNames}.";
+  
+  _ = assert builtins.hasAttr systemType types;
+    "Invalid systemType: ${systemType}. It must be one of ${builtins.attrNames types}.";
+
+  # Generate the configuration based on systemName and systemType
+  generateConfig = {
+    imports = [
+      ./common.nix
+      ./types/${systemType}/imports.nix
+      ./types/${systemType}/home.nix
+    ];
+
+    hostname = "${systemName}-${systemType}";
+    networking.hostName = "${systemName}-${systemType}";
+  };
+
+in {
+  imports = [ generateConfig ];
 }
--- users.nix
+++ users.nix
@@ -1,26 +1,41 @@
-{config, pkgs, ...}:
-{
-  users = {
-    master = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ]; 
-      hashedPassword = "4b52f55fd458d57b645cd85bcbf1ad5b3628fa07add9b0e93e3fec5ca621c14a"; # Replace with hashed password
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3Nza... your_key_comment" # Replace with public key
-      ];
-    };
-    node = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ]; 
-      hashedPassword = "4b52f55fd458d57b645cd85bcbf1ad5b3628fa07add9b0e93e3fec5ca621c14a"; # Replace with hashed password
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3Nza... your_key_comment" # Replace with public key
-      ];
-    };
-    user = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "disk" "audio" "video"]; 
-      hashedPassword = "4b52f55fd458d57b645cd85bcbf1ad5b3628fa07add9b0e93e3fec5ca621c14a"; # Replace with hashed password
-    };
-  };
+{ config, pkgs, ... }:
+
+let
+  # List of all system names
+  systemNames = [
+    "sys1" "sys2" "sys3" "sys4" "sys5"
+    "sys6" "sys7" "sys8" "sys9" "sys10"
+    "sys11" "sys12" "sys13" "sys14" "sys15"
+    "sys16" "sys17" "sys18" "sys19" "sys20"
+    "sys21" "sys22" "sys23" "sys24" "sys25"
+  ];
+
+  # Define the available types
+  types = [ "master" "node" "user" ];
+
+  # Generate only users for the "user" type
+  userConfigurations = builtins.concatMap (systemName:
+    builtins.map (systemType:
+      if systemType == "user" then
+        {
+          name = "${systemName}-${systemType}";
+          isNormalUser = true;
+          description = "User for ${systemName}-${systemType}";
+          extraGroups = [ "wheel" "networkmanager" ];  # Add relevant groups here
+          home = "/home/${systemName}-${systemType}";
+          shell = pkgs.bash;  # Set default shell
+        }
+      else null
+    ) types
+  ) systemNames;
+
+  # Filter out null entries from userConfigurations
+  validUserConfigurations = builtins.filter (x: x != null) userConfigurations;
+
+in {
+  # Add all the generated users to the configuration
+  users.users = builtins.listToAttrs (builtins.map (user: {
+    name = user.name;
+    value = user;
+  }) validUserConfigurations);
 }
