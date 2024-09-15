6faed441d11bcc5c4000b5d6e5716a08122e5afa
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-15 12:07:33 +0200

    updated base.nix

--- previous iterations/base.nix
+++ previous iterations/base.nix
@@ -52,13 +52,9 @@
  
 
 
-  users.users.HL_Node = {
+  users.users.sys1-user = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable sudo
-    hashedPassword = "4b52f55fd458d57b645cd85bcbf1ad5b3628fa07add9b0e93e3fec5ca621c14a"; # Replace with hashed password
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3Nza... your_key_comment" # Replace with public key
-    ];
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable sudo
   };
 
  
