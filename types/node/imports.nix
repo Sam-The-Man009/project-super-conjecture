9b8a1efde1087448026722ea9e7e40e640ecdfd7
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-09-21 22:34:52 +0200

    importing less to find where the erros are coming from

--- types/node/imports.nix
+++ types/node/imports.nix
@@ -3,6 +3,6 @@
 {
   imports = [
     (import ./pkgs.nix { inherit config pkgs; })
-    (import ./services.nix { inherit config pkgs; })
+    #(import ./services.nix { inherit config pkgs; })
   ];
 }
