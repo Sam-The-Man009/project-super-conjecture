d99f6130c58040d3070f80ab7554a3f7961ead07
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-10-17 14:55:48 +0200

    Update home.nix

--- types/user/home.nix
+++ types/user/home.nix
@@ -13,7 +13,7 @@
 
     programs.zsh = {
       enable = true;
-      sessionInit = ''source ${config.users.user.home.homeDirectory}//.zshrc'';
+      sessionInit = ''source ${config.homeManager.users.user.home.homeDirectory}//.zshrc'';
       
       aliases = {
         sys-rebuild = "sudo nixos-rebuild switch --flake /path/to/nixos-config";
