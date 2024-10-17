e4da0b60ee8eb2212609419bb500f82e39b4de29
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-10-17 14:33:08 +0200

    updated homes, because i missunderstood how config works (:

--- types/node/home.nix
+++ types/node/home.nix
@@ -1,5 +1,6 @@
 { pkgs, config, ... }: {
 
+
   homeManager.users.node = {
     home = {
     homeDirectory = "/home/node";
@@ -13,7 +14,7 @@
 
     programs.zsh = {
       enable = true;
-      sessionInit = "source ${config.home.homeDirectory}/.zshrc";
+      sessionInit = "source ${homeManager.users.node.home.homeDirectory}/.zshrc";
       
       aliases = {
         sys-rebuild = "sudo nixos-rebuild switch --flake /path/to/nixos-config";
