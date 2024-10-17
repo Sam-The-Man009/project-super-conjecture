75fac33bc97b99c76d8d3a40d4b80ea858f94dc3
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2024-10-17 18:39:23 +0200

    changes to temp boot

--- bootloader.nix
+++ bootloader.nix
@@ -16,7 +16,7 @@
 #  boot.loader.grub.efiSupport = true;
 #  boot.loader.grub.efiInstallAsRemovable = true;
 #  boot.loader.efi.canTouchEfiVariables = true;
-#  boot.loader.grub.devices = [ "nodev" ]; #change the nodev
+#  boot.loader.grub.devices = [ "nodev" ];
 #  boot.loader.grub.useOSProber = true;
 
 
@@ -26,6 +26,8 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.useOSProber = true;
 
 
 }
